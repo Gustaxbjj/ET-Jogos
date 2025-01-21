6be7022347d45e7e759ae4de7a60269250dd7056
Gabrielluiz998 <153648794+Gustaxbjj@users.noreply.github.com>
2025-01-21 07:40:47 -0300

    ...

--- 4o-bm/sequelize/model/Genero.js
+++ 4o-bm/sequelize/model/Genero.js
@@ -3,7 +3,16 @@ const database = require('../db');
 
 const Genero = database.define(
     'Genero',
-    {
-        
-    }
-)
+   {
+
+       cod:{
+            type: DataType.STRING,
+            autoIncrement: false,
+            allowNull: false,
+            primaryKey: true
+        },
+        nome:{
+            type:DataType.STRING,
+            allowNull:true
+        }
+   })
